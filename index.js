8b13a586786d5e62cd097e96527e29620eb218c1
shefattonmoy <shefathossain7@gmail.com>
2023-09-26 03:34:25 +0600

    news json done

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-const categories = require('./Data/categories.json');
+const categories = require("./Data/categories.json");
+const news = require("./Data/news.json");
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send("The Dragon News is Running");
+app.get("/", (req, res) => {
+  res.send("The Dragon News is Running");
 });
 
-app.get('/categories', (req, res) => {
-    console.log(categories);
-    res.send(categories);
+app.get("/categories", (req, res) => {
+  res.send(categories);
+});
+
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
+
+app.get("/categories/:id", (req, res) => {
+  const id = req.params.id;
+  if (id == 0) {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+  }
 });
 
 app.listen(port, () => {
-    console.log(`The Dragon News API is Running on port: ${port}`);
+  console.log(`The Dragon News API is Running on port: ${port}`);
 });
